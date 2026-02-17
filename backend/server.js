2d743827d9144cd196fb45eb33975ab34b540d88
pathofvasu <vu24fa04873@gmail.com>
2026-02-17 09:04:19 +0530

    Backend: Implemented login API with JWT authentication

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("./models/User");
-require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -45,6 +47,41 @@ app.post("/api/register", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Server Error", error });
   }
+});
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // 2. Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // 3. Create JWT token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (error) {
+  console.error("LOGIN ERROR:", error);
+  res.status(500).json({ message: "Server Error" });
+}
+
 });
 
 
